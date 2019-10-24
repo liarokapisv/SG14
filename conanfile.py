3ef8332cf11e5827afea6db808b34b48962a1b5f
Alexandros Liarokapis <liarokapis.v@gmail.com>
2019-10-24 13:10:55 +0300

    removed unneeded options

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,6 @@ from conans import ConanFile, CMake
 class Sg14Conan(ConanFile):
     name = "sg14"
     version = "0.1"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
     exports_sources = "CMakeLists.txt", "SG14/*"
 
     def package(self):
